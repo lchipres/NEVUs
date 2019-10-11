9c32001b8585148405f0188f1687f994652a1bdb
Leonardo <lchipres@ucol.mx>
2019-10-11 08:26:16 -0500

    Added register screen needs design, database connection

---
+++ NEVUs/screens/RegisterScreen.tsx
@@ -0,0 +1,153 @@
+import * as React from "react";
+import { StyleSheet, View, Image,KeyboardAvoidingView } from "react-native";
+import Button from "../components/Button";
+import FormTextInput from "../components/FormTextInput";
+import strings from "../config/strings";
+import constants from "../config/constants";
+
+interface State {
+    email: string;
+    password: string;
+    confirmPassword:string;
+    emailTouched: boolean;
+    passwordTouched: boolean;
+  }
+
+class RegisterScreen extends React.Component<{},State>{
+    passwordInputRef= React.createRef<FormTextInput>();
+    passwordConRef= React.createRef<FormTextInput>();
+  
+  readonly state: State = {
+    email: "",
+    password:"",
+    emailTouched:false,
+    passwordTouched:false,
+    confirmPassword:""
+  };
+
+  handleEmailChange = (email: string) =>{
+    this.setState({email:email});
+  };
+
+  handlePasswordChange = (password: string) =>{
+    this.setState({password:password});
+  };
+
+  handlePasswordConChange = (confirmPassword:string)=>{
+    this.setState({confirmPassword:confirmPassword})
+  };
+
+  handleRegisterPress = () => {
+    console.log("Register button pressed");
+  };
+
+  handleEmailSubmitPress = () => {
+    if (this.passwordInputRef.current){
+      this.passwordInputRef.current.focus();
+    }
+  };
+
+  handlePasswordSubmitPress = () =>{
+        if (this.passwordConRef.current){
+        this.passwordConRef.current.focus();
+      }
+  };
+
+  handleEmailBlur = () => {
+    this.setState({emailTouched:true});
+  };
+
+  handlePasswordBlur = () => {
+    this.setState({passwordTouched:true});
+  };
+
+
+    render(){
+      const {
+        email,
+        password,
+        emailTouched,
+        passwordTouched
+      } = this.state;
+
+      const emailError =
+        !email && emailTouched
+          ? strings.EMAIL_REQUIERED
+          : undefined;
+      const passwordError = 
+        !password && passwordTouched
+          ? strings.PASSWORD_REQUIERED
+          : undefined;
+    return (
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior="padding"
+      >
+      <View style={styles.container}>
+          <Image source={require('../assets/images/Nevus_logo.png')}  
+          style={ styles.logo} />
+        <View style={styles.form}>
+          <FormTextInput
+            value = {this.state.email}
+            onChangeText={this.handleEmailChange}
+            placeholder={strings.EMAIL_PLACEHOLDER}
+            onSubmitEditing={this.handleEmailSubmitPress}
+            autoCorrect={false}
+            keyboardType="email-address"
+            returnKeyType="next"
+            onBlur={this.handleEmailBlur}
+            error={emailError}
+            blurOnSubmit={constants.IS_IOS}
+          />
+          <FormTextInput
+            ref={this.passwordInputRef}
+            value = {this.state.password}
+            onChangeText={this.handlePasswordChange}
+            placeholder={strings.PASSWORD_PLACEHOLDER}
+            secureTextEntry={true}
+            returnKeyType="next"
+            onBlur={this.handlePasswordBlur}
+            error={passwordError}
+          />
+          <FormTextInput
+            ref={this.passwordConRef}
+            value = {this.state.password}
+            onChangeText={this.handlePasswordChange}
+            placeholder={strings.PASSWORD_PLACEHOLDER}
+            secureTextEntry={true}
+            returnKeyType="done"
+            onBlur={this.handlePasswordBlur}
+            error={passwordError}
+          />
+          <Button label={strings.REGISTER} onPress={this.handleRegisterPress}
+          disabled={!email||!password||!this.passwordConRef}
+          />
+        </View>
+      </View>
+      </KeyboardAvoidingView>
+    );
+    }
+  }
+  
+  const styles = StyleSheet.create({
+    container:{
+      flex: 1,
+      width:"100%",
+      height:"100%",
+      alignItems: "center",
+      justifyContent: "space-between"
+    },
+    logo: {
+      flex:1,
+      width:"100%",
+      resizeMode:"contain",
+      alignSelf:"center"
+    },
+    form: {
+      flex:1,
+      alignSelf:"center",
+      justifyContent:"center",
+      width:"80%"
+    }
+  });
+}
